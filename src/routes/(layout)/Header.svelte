9944980887ae302ef4025c5ca86e3e324c898a4d
Thanish Nizam <mnmtanish@gmail.com>
2025-08-02 04:42:37 +0200

    feat: fix the logo url on header

--- src/routes/(layout)/Header.svelte
+++ src/routes/(layout)/Header.svelte
@@ -1,14 +1,14 @@
+<script lang="ts">
+	import logoImage from '$lib/assets/logo-256.png';
+</script>
+
 <header
 	class="sticky top-0 z-50 w-full border-b border-white/10 bg-white/80 backdrop-blur-xl supports-[backdrop-filter]:bg-white/60"
 >
 	<div class="container flex h-20 items-center justify-between px-4 md:px-6 lg:px-8">
 		<div class="flex items-center space-x-3">
 			<div class="h-10 w-10 overflow-hidden rounded shadow-lg">
-				<img
-					src="/src/lib/assets/logo-256.png"
-					alt="Knivsta Moske Logo"
-					class="h-full w-full object-cover"
-				/>
+				<img src={logoImage} alt="Knivsta Moske Logo" class="h-full w-full object-cover" />
 			</div>
 			<div>
 				<span
